8d4cdcbad54741da2e624982b063ab740134b99d
Satabdi Sangma <73372007+SatabdiSam@users.noreply.github.com>
2022-04-05 16:50:31 -0400

    commit 1
    initial commit

---
+++ src/app/missiondetails/missiondetails.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Mission } from '../models/mission';
+import { SpacexapiService } from '../network/spacexapi.service';
+
+@Component({
+
+  selector: 'app-missiondetails',
+  templateUrl: './missiondetails.component.html',
+  styleUrls: ['./missiondetails.component.css']
+})
+
+export class MissiondetailsComponent implements OnInit {
+  mission?:Mission;
+
+  constructor(private dataService:SpacexapiService, private route:ActivatedRoute) {
+    this.route.params.subscribe(params=>
+      this.getMission(params['id'])
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+  getMission(id:number){
+    this.dataService.getMission(id).subscribe(data=>{
+      this.mission = data;
+    });
+  }
+}
---
+++ src/app/missionlist/missionlist.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Mission } from '../models/mission';
+import { SpacexapiService } from '../network/spacexapi.service';
+
+@Component({
+
+  selector: 'app-missionlist',
+  templateUrl: './missionlist.component.html',
+  styleUrls: ['./missionlist.component.css']
+})
+
+export class MissionlistComponent implements OnInit {
+
+  missions:Mission[] = [];
+
+  constructor(private dataService:SpacexapiService) {
+    this.getMissionList();
+  }
+
+  ngOnInit(): void {
+  }
+
+  getMissionList(){
+    
+    this.dataService.getMissionList().subscribe(data=>{
+      this.missions = data;
+    });
+  }
+}
